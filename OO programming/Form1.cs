d63195e9f2cfbbb0f316e2f385157c308be58770
Bryce Taylor <>
2022-11-28 12:56:29 -0300

    Clean up code and bump .NET Framework version

--- OO programming/Form1.cs
+++ OO programming/Form1.cs
@@ -1,43 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
-namespace OO_programming
+namespace OO_programming;
+
+internal partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            // Add code below to complete the implementation to populate the listBox
-            // by reading the employee.csv file into a List of PaySlip objects, then binding this to the ListBox.
-            // CSV file format: <employee ID>, <first name>, <last name>, <hourly rate>,<taxthreshold>
+        // Add code below to complete the implementation to populate the listBox
+        // by reading the employee.csv file into a List of PaySlip objects, then binding this to the ListBox.
+        // CSV file format: <employee ID>, <first name>, <last name>, <hourly rate>,<taxthreshold>
 
-        }
+    }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // Add code below to complete the implementation to populate the
-            // payment summary (textBox2) using the PaySlip and PayCalculatorNoThreshold
-            // and PayCalculatorWithThresholds classes object and methods.
+    private void Button1_Click(object sender, EventArgs e)
+    {
+        // Add code below to complete the implementation to populate the
+        // payment summary (textBox2) using the PaySlip and PayCalculatorNoThreshold
+        // and PayCalculatorWithThresholds classes object and methods.
 
-        }
+    }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // Add code below to complete the implementation for saving the
-            // calculated payment data into a csv file.
-            // File naming convention: Pay_<full name>_<datetimenow>.csv
-            // Data fields expected - EmployeeId, Full Name, Hours Worked, Hourly Rate, Tax Threshold, Gross Pay, Tax, Net Pay, Superannuation
+    private void Button2_Click(object sender, EventArgs e)
+    {
+        // Add code below to complete the implementation for saving the
+        // calculated payment data into a csv file.
+        // File naming convention: Pay_<full name>_<datetimenow>.csv
+        // Data fields expected - EmployeeId, Full Name, Hours Worked, Hourly Rate, Tax Threshold, Gross Pay, Tax, Net Pay, Superannuation
 
-        }
     }
 }
--- OO programming/Program.cs
+++ OO programming/Program.cs
@@ -1,57 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
-namespace OO_programming
+namespace OO_programming;
+
+file static class Program
 {
-
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-
-
     /// <summary>
-    /// Class a capture details accociated with an employee's pay slip record
+    /// The main entry point for the application.
     /// </summary>
-    public class PaySlip
+    [STAThread]
+    private static void Main()
     {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
     }
+}
 
-    /// <summary>
-    /// Base class to hold all Pay calculation functions
-    /// Default class behaviour is tax calculated with tax threshold applied
-    /// </summary>
-    public class PayCalculator
-    {
-     
-    }
+/// <summary>
+/// Class a capture details accociated with an employee's pay slip record
+/// </summary>
+file sealed record PaySlip
+{
+}
 
-    /// <summary>
-    /// Extends PayCalculator class handling No tax threshold
-    /// </summary>
-    public class PayCalculatorNoThreshold : PayCalculator
-    {
-     
-    }
+/// <summary>
+/// Base class to hold all Pay calculation functions
+/// Default class behaviour is tax calculated with tax threshold applied
+/// </summary>
+file abstract class PayCalculator
+{
+}
 
-    /// <summary>
-    /// Extends PayCalculator class handling With tax threshold
-    /// </summary>
-    public class PayCalculatorWithThreshold : PayCalculator
-    {
-     
-    }
+/// <summary>
+/// Extends PayCalculator class handling No tax threshold
+/// </summary>
+file sealed class PayCalculatorNoThreshold : PayCalculator
+{
+}
+
+/// <summary>
+/// Extends PayCalculator class handling With tax threshold
+/// </summary>
+file sealed class PayCalculatorWithThreshold : PayCalculator
+{
 }
